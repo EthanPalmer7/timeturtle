151b9bdbb1dfbbb68a060dddc6451d36ae8eb20a
Ethan Palmer <eep1211@gmail.com>
2023-10-28 13:04:35 -0700

    working on js

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
+const daysContainer = document.querySelector(".days"),
+  nextBtn = document.querySelector(".next-btn"),
+  prevBtn = document.querySelector(".prev-btn"),
+  month = document.querySelector(".month");
+
+// <!-- prev month days -->
+// <div class="day prev">28</div>
+// <div class="day prev">29</div>
+// <div class="day prev">30</div>
+// <!-- current month days -->
+// <div class="day">1</div>
+// <div class="day">2</div>
+// <div class="day">3</div>
+// <div class="day today">4</div>
+// <div class="day">5</div>
+// <div class="day">6</div>
+// <div class="day">7</div>
+// <!-- next month days -->
+// <div class="day next">1</div>
+// <div class="day next">2</div>
+// <div class="day next">3</div>
+
 const months = [
   "January",
   "February",
@@ -14,3 +36,25 @@ const months = [
 ];
 
 const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+// get current date
+const date = new Date();
+
+// get current month
+let currentMonth = date.getMonth();
+
+// get current year
+let currentYear = date.getFullYear();
+
+// function to render days
+function renderCalendar() {
+  // get prev month, current month, and next month days
+  date.setDate(1);
+  const firstDay = new Date(currentYear, currentMonth, 1);
+  const lastDay = new Date(currentYear, currentMonth + 1, 0);
+  const lastDayIndex = lastDay.getDay();
+  const lastDayDate = lastDay.getDate();
+  const prevlastDay = new Date(currentYear, currentMonth, 0);
+  const prevLastDayDate = prevLastDay.getDate();
+  const nextDays = 7 - lastDayIndex - 1;
+}
