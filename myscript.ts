b3610fbef4bb742cfbe606ba922917ac4a6b8fa5
Ethan Palmer <eep1211@gmail.com>
2023-11-04 18:28:18 -0700

    added mroe comments finaly adding more to cal

--- myscript.ts
+++ myscript.ts
@@ -24,53 +24,59 @@ const months: string[] = [
 // title for each column
 const days: string[] = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
-/**
- * Date()
- * returns a string representation of the current date and time
- * i.e. --> "Wed Nov 01 2023 12:28:59 GMT-0700 (Pacific Daylight Time)"
- */
+// i.e. --> date == "Wed Nov 01 2023 12:28:59 GMT-0700 (Pacific Daylight Time)"
 const date: Date = new Date();
 
-/**
- * getMonth()
- * returns int between 0-11 inclusive representing current month
- * i.e. --> 10 (November)
- */
+// int between 0-11 inclusive representing current month
 let currentMonth: number = date.getMonth();
 
-/**
- * getFullYear()
- * returns int representing current year
- * i.e. --> 2023
- */
+// currentYear == 2023
 let currentYear: number = date.getFullYear();
 
 // function to render days
 function renderCalendar(): void {
   // get prev month, current month, and next month days
   date.setDate(1);
+
+  // i.e. --> firstDay == "Wed Nov 01 2023 00:00:00 GMT-0700 (Pacific Daylight Time)"
   const firstDay: Date = new Date(currentYear, currentMonth, 1);
+
+  // i.e. --> lastDay == "Thu Nov 30 2023 00:00:00 GMT-0800 (Pacific Standard Time)"
   const lastDay: Date = new Date(currentYear, currentMonth + 1, 0);
+
+  // i.e. --> lastDayIndex == (0 Sunday, 6 Saturday)
   const lastDayIndex: number = lastDay.getDay();
+
+  // i.e. --> lastDayDate == (1-31)
   const lastDayDate: number = lastDay.getDate();
+
+  // i.e. --> prevLastDay == "Tue Oct 31 2023 00:00:00 GMT-0700 (Pacific Daylight Time)"
   const prevLastDay: Date = new Date(currentYear, currentMonth, 0);
+
+  // i.e. --> prevLastDayDate == (1-31)
   const prevLastDayDate: number = prevLastDay.getDate();
+
+  // number of days to display after the last day (if lastday of the month is saturday, index == 6, and nextdays == 0)
   const nextDays: number = 7 - lastDayIndex - 1;
 
   // update current year and month in header
   month.innerHTML = `${months[currentMonth]} ${currentYear}`;
 
-  // update days html
+  //
   let days: string = "";
 
-  // prev days html
+  // get day of week for 1st day of month (we want to know if it is Wed (3) etc..)
+  // if day is sunday skip, else add prev month days to HTMLElement to days String
+  // iterate through days before 1st day untill sunday (i.e. --> if 1st day is wednesday, iterate 3 times (Sun, Mon, Tues))
+  // add prev days to days string as HTMLELement with added class tag 'prev'
   for (let x: number = firstDay.getDay(); x > 0; x--) {
+    // add prev days starting from sunday to day before 1st day
     days += `<div  class="day prev">${prevLastDayDate - x + 1}</div>`;
   }
 
-  // current month days
+  // loop through days in month, if i == current day of month add class tag 'today', else dont add class tag 'today'
+  // add all 28+ days as HTMLElements to days string
   for (let i: number = 1; i <= lastDayDate; i++) {
-    // check if its oday then add today class
     if (
       i === new Date().getDate() &&
       currentMonth === new Date().getMonth() &&
@@ -84,13 +90,16 @@ function renderCalendar(): void {
     }
   }
 
-  // next month days
+  // loop thorugh days after last day
+  // add next days to days string as HTMLELement with added class tag 'next'
   for (let j: number = 1; j <= nextDays; j++) {
     days += `<div class="day next">${j}</div>`;
   }
 
   // run these functions with every calendar render
   hideTodayBtn();
+
+  // add to the innerHTML of the days class (daysContainter variable) to display all days (prev, currentmonth, today, next)
   daysContainer.innerHTML = days;
 }
 
@@ -143,3 +152,9 @@ function hideTodayBtn(): void {
     todayBtn.style.display = "flex";
   }
 }
+
+const daysChildContainter = daysContainer.querySelectorAll(".day");
+
+daysChildContainter.forEach((daysChild) => {
+  daysChild.addEventListener("click", () => {});
+});
